434ff9c4837e93854727c3c6aa73f92e75d2b3f8
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2024-12-22 16:41:36 +0530

    first

---
+++ src/main/java/com/jeewaeducation/application_tracking/ApplicationTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.jeewaeducation.application_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeewaeducation/application_tracking/ApplicationTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeewaeducation.application_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
