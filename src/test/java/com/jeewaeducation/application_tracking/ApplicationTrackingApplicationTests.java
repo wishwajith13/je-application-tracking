98f5e34e531ffeb5875973e6eb7d846df01218e4
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2025-01-25 09:23:13 +0530

    docker setup

--- src/test/java/com/jeewaeducation/application_tracking/ApplicationTrackingApplicationTests.java
+++ src/test/java/com/jeewaeducation/application_tracking/ApplicationTrackingApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jeewaeducation.application_tracking;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+
 class ApplicationTrackingApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Configuration
+	static class TestConfig {
+		@Bean
+		public AmazonS3 amazonS3() {
+			return Mockito.mock(AmazonS3.class); // Provide a mocked bean
+		}
+	}
 }
